01c8a7ccba774252d66abca4f4f14a0c49f2dc65
eben92 <pappy.rhutmannjnrolege@gmail.com>
2024-04-27 02:03:46 +0000

    first commit

---
+++ app/(lobby)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "@/components/Themed";
+import { SafeAreaView } from "react-native";
+
+export default function Lobby() {
+  return (
+    <SafeAreaView>
+      <Text>Tab One</Text>
+    </SafeAreaView>
+  );
+}
